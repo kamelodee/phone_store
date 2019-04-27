b343a7ebb6ea2df76223220c9ebb95f7562ec9c3
mamudu kamilo <kamelodee@gmail.com>
2019-04-27 21:29:22 +0000

    Updates

---
+++ precache-manifest.00cbf3fc29d395ef25815569cd3f8715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d805ab0529202ca0cd4c3e6941a5f5",
+    "url": "/phone_store/index.html"
+  },
+  {
+    "revision": "e0f7cde6e9e72398f090",
+    "url": "/phone_store/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c3c163e1b077e1bcbd98",
+    "url": "/phone_store/static/css/main.ceb9dccd.chunk.css"
+  },
+  {
+    "revision": "e0f7cde6e9e72398f090",
+    "url": "/phone_store/static/js/2.1eca6ea0.chunk.js"
+  },
+  {
+    "revision": "c3c163e1b077e1bcbd98",
+    "url": "/phone_store/static/js/main.689756f9.chunk.js"
+  },
+  {
+    "revision": "d55390e581ae1e575859",
+    "url": "/phone_store/static/js/runtime~main.4f3639c7.js"
+  },
+  {
+    "revision": "b0fc75ce4479b794ca9bccc3b367ee72",
+    "url": "/phone_store/static/media/logo.b0fc75ce.svg"
+  }
+]);
